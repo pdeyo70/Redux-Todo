49c4bace6fe6c47624f4e539a5e0502159fc7fc0
Paul DeYonghe <p.deyonghe@gmail.com>
2019-05-09 20:50:20 -0500

    starting out; replaced the standard react app with a simple, humorous <h1>

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>There's fixin' to be a whole mess of todos up in here!</h1>
+    </div>
+  );
+}
+
+export default App;
